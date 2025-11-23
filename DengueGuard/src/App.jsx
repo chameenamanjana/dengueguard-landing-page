5a4102464b70e69eb3449e70c6fa9129393085e8
Emika Sandina <emikasandina06@gmail.com>
2025-11-23 21:26:10 +0530

    added about

--- DengueGuard/src/App.jsx
+++ DengueGuard/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
 import './App.css'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
@@ -10,6 +12,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <About/>
     </>
   )
 }
---
+++ DengueGuard/src/components/About/About.jsx
@@ -0,0 +1,46 @@
+import "./about.css";
+
+function About() {
+  return (
+    <>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h2 className="main-heading">About Us</h2>
+      <div className="container">
+        <div className="about">
+          <div className="about-text">
+            <h2 className="main-title">What is DengueGuard?</h2>
+            <br></br>
+
+            <p>
+              Dengue fever is considered as a significantly growing health
+              challenge in Sri Lanka. The current reactive approach taken to
+              mitigate dengue disease leads to delayed detection and response
+              which allows the disease to spread rapidly.
+            </p>
+            <br></br>
+            <p>
+              DengueGuard, an AI powered and community driven web platform
+              designed to transform dengue mitigation from a reactive to a
+              proactive approach.
+            </p>
+            <br></br>
+            <p> DengueGuard provides the ability to
+              citizens to report dengue breeding grounds and suspected symptoms,
+              access heatmaps and engage with an AI chatbot for prevention
+              advice. Health Officers utilises a web dashboard to manage citizen
+              reports, view heatmap and send special announcements to citizens.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
